6f157374855aab09816a4db7a1ff2ee4e4fede81
Aayush <aayushbatham210036@acropolis.in>
2023-10-09 18:22:24 +0530

    new

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -5,7 +5,8 @@ import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-// import "./header.css";
+import "./header.css";
+import "../app/globals.css";
 
 import Logo from "app/logo.png";
 import betting from "./betting.png";
