dd02fa03ce81b4405ca31e8cad8369e96fa0d2de
Aayush <aayushbatham210036@acropolis.in>
2023-10-10 00:52:11 +0530

    done

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -27,203 +27,92 @@ const Header = () => {
   const [navbar, setNavbar] = useState(false);
   return (
     <main className="bg-transparent">
-      <header className="lg:h-[8vh] h-[7vh] flex justify-center items-center fixed w-[100vw] z-[9999] nav-bg">
-        <nav className="flex items-center justify-between w-[95%] md:w-[82%] ">
-          <div id="logo" className="scale-[0.85] lg:scale-100 relative z-[100]">
-            <Link href="/" className="flex items-center gap-2">
-              {/* <img src="logo.png" alt="logo" className="h-[80%]" /> */}
-              <Image src={Logo} alt="logo" width={50} />
-              <p className="text-xl font-extrabold">Xettle</p>
-            </Link>
-          </div>
-          <div id="links" className="flex items-center justify-between">
-            <ul
-              className={`nav-links fixed gap-[0.5vw] transition-all duration-500 block lg:static lg:inline-flex lg:h-auto lg:w-auto lg:max-w-none left-0 h-[100vh] w-[100%] max-w-[350px} overflow-y-auto lg:top-0 ${
-                navbar ? "lgnav" : "top-[-110vh] "
-              }`}
+      <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="https://flowbite.com/" class="flex items-center">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              class="h-8 mr-3"
+              alt="Flowbite Logo"
+            />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Xettle
+            </span>
+          </a>
+          <div class="flex md:order-2">
+            <button
+              type="button"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              <li className="rounded-lg hover:bg-[rgba(216,_216,_216,_0.59)] lg:px-[1.4rem] lg:py-[0.2rem] transition-all text-[4rem] lg:text-[1rem]">
-                <Link href="/" onClick={() => setNavbar(!navbar)}>
+              Get started
+            </button>
+            <button
+              data-collapse-toggle="navbar-sticky"
+              type="button"
+              class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-sticky"
+              aria-expanded="false"
+            >
+              <span class="sr-only">Open main menu</span>
+              <svg
+                class="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+          </div>
+          <div
+            class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-sticky"
+          >
+            <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                  aria-current="page"
+                >
                   Home
-                </Link>
+                </a>
               </li>
-              <li className="rounded-lg lg:hover:bg-[rgba(216,_216,_216,_0.59)] lg:px-[1.4rem] lg:py-[0.2rem] transition-all text-[4rem] lg:text-[1rem]">
-                <p className="cursor-pointer whitespace-nowrap">
-                  Services &#8595;
-                </p>
-                <div className="static hidden transition-all opacity-0 dropdown ">
-                  <div className="absolute lg:h-10 lg:w-[40rem] lg:translate-x-[-30%] transition-all"></div>
-
-                  <div className={lexend.className}>
-                    <div className=" bg-transparent relative transition-all shadow-[0px_4px_35px_0px_rgba(0,_0,_0,_0.15)] lg:absolute lg:min-w-[40rem] lg:min-h-[15rem] lg:translate-x-[-45%] lg:flex lg:justify-evenly lg:items-center lg:gap-10 lg:rounded-2xl lg:mt-10 lg:bg-white ">
-                      <ul className="lg:w-[40%]">
-                        <li className="flex items-center justify-start rounded-lg hover:bg-[rgba(216,_216,_216,_0.59)] lg:px-[1.4rem] lg:py-[0.2rem] transition-all my-3 text-[1rem]">
-                          <Image
-                            src={crypto}
-                            alt="crypto"
-                            width={45}
-                            className="hidden lg:block"
-                          />
-                          <Link
-                            href="/crypto-excng"
-                            className="px-3"
-                            onClick={() => setNavbar(!navbar)}
-                          >
-                            Crypto Exchange Development
-                          </Link>
-                        </li>
-                        <li className="flex items-center justify-start rounded-lg hover:bg-[rgba(216,_216,_216,_0.59)] lg:px-[1.4rem] lg:py-[0.2rem] transition-all my-3 text-[1rem]">
-                          <Image
-                            src={fintech}
-                            alt="fintech"
-                            width={45}
-                            className="hidden lg:block"
-                          />
-                          <Link
-                            href="/fintech"
-                            className="px-3"
-                            onClick={() => setNavbar(!navbar)}
-                          >
-                            Fin-Tech Software
-                          </Link>
-                        </li>
-                        <li className="flex items-center justify-start rounded-lg hover:bg-[rgba(216,_216,_216,_0.59)] lg:px-[1.4rem] lg:py-[0.2rem] transition-all my-3 text-[1rem]">
-                          <Image
-                            src={game}
-                            alt="game"
-                            width={45}
-                            className="hidden lg:block"
-                          />
-                          <Link
-                            href="/games"
-                            className="px-3"
-                            onClick={() => setNavbar(!navbar)}
-                          >
-                            Games of Skill And Chances
-                          </Link>
-                        </li>
-                        <li className="flex items-center justify-start rounded-lg hover:bg-[rgba(216,_216,_216,_0.59)] lg:px-[1.4rem] lg:py-[0.2rem] transition-all my-3 text-[1rem]">
-                          <Image
-                            src={casino}
-                            alt="casino"
-                            width={45}
-                            className="hidden lg:block"
-                          />
-                          <Link
-                            href="/casino"
-                            className="px-3"
-                            onClick={() => setNavbar(!navbar)}
-                          >
-                            Crypto Casino Development
-                          </Link>
-                        </li>
-                      </ul>
-                      <ul className="lg:w-[40%]">
-                        <li className="flex items-center justify-start rounded-lg hover:bg-[rgba(216,_216,_216,_0.59)] lg:px-[1.4rem] lg:py-[0.2rem] transition-all my-3 text-[1rem]">
-                          <Image
-                            src={betting}
-                            alt="bettng"
-                            width={45}
-                            className="hidden lg:block"
-                          />
-                          <Link
-                            href="/betting"
-                            className="px-3"
-                            onClick={() => setNavbar(!navbar)}
-                          >
-                            Sports / fantasy betting Softwares
-                          </Link>
-                        </li>
-                        <li className="flex items-center justify-start rounded-lg hover:bg-[rgba(216,_216,_216,_0.59)] lg:px-[1.4rem] lg:py-[0.2rem] transition-all my-3 text-[1rem]">
-                          <Image
-                            src={token}
-                            alt="token"
-                            width={40}
-                            className="hidden lg:block"
-                          />
-                          <Link
-                            href="/crypto-token"
-                            className="px-3"
-                            onClick={() => setNavbar(!navbar)}
-                          >
-                            Crypto Project / Token development
-                          </Link>
-                        </li>
-                        <li className="flex items-center justify-start rounded-lg hover:bg-[rgba(216,_216,_216,_0.59)] lg:px-[1.4rem] lg:py-[0.2rem] transition-all my-3 text-[1rem]">
-                          <Image
-                            src={ecom}
-                            alt="ecom"
-                            width={50}
-                            className="hidden lg:block"
-                          />
-                          <Link
-                            href="/ecom"
-                            className="px-3"
-                            onClick={() => setNavbar(!navbar)}
-                          >
-                            E-comm development
-                          </Link>
-                        </li>
-                        <li className="flex items-center justify-start rounded-lg hover:bg-[rgba(216,_216,_216,_0.59)] lg:px-[1.4rem] lg:py-[0.2rem] transition-all my-3 text-[1rem]">
-                          <Image
-                            src={payment}
-                            alt="payment"
-                            width={45}
-                            className="hidden lg:block"
-                          />
-                          <Link
-                            href="/payment"
-                            className="px-3"
-                            onClick={() => setNavbar(!navbar)}
-                          >
-                            Payment Gateway
-                          </Link>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  About
+                </a>
               </li>
-              <li className="rounded-lg hover:bg-[rgba(216,_216,_216,_0.59)] lg:px-[1.4rem] lg:py-[0.2rem] transition-all text-[4rem] lg:text-[1rem]">
-                <Link
-                  href="/contact"
-                  rel="preload"
-                  onClick={() => setNavbar(!navbar)}
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Services
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Contact
-                </Link>
-              </li>
-              <li className="rounded-lg hover:bg-[rgba(216,_216,_216,_0.59)] lg:px-[1.4rem] lg:py-[0.2rem] transition-all text-[4rem] lg:text-[1rem]">
-                <Link
-                  href="/about"
-                  className=" whitespace-nowrap"
-                  onClick={() => setNavbar(!navbar)}
-                >
-                  About Us
-                </Link>
+                </a>
               </li>
             </ul>
           </div>
-          <div id="cta" className="flex items-center">
-            {/* <button className="w-32 h-10 bg-[#7EFE34] rounded-[0.5rem] scale-[0.85] lg:scale-100">
-              Book a Demo
-            </button> */}
-            <Link
-              href="/contact"
-              className=" px-[0.7rem] py-[0.7rem] rounded-lg text-white mx-auto bg-[#7EFE34] "
-            >
-              Book a Demo
-            </Link>
-            <button
-              onClick={() => setNavbar(!navbar)}
-              id="menu-icon"
-              className="relative inline-flex z-[1000]"
-            >
-              {navbar ? <X /> : <Ham />}
-            </button>
-          </div>
-        </nav>
-      </header>
+        </div>
+      </nav>
     </main>
   );
 };
